c1f84112eebfa0cf5e8ed77706458e40aa0afc3a
GustavBoe <gustavhugoboe@icloud.com>
2026-06-02 10:34:36 +0200

    Styled the project images for desktop.

--- src/features/HomeView.jsx
+++ src/features/HomeView.jsx
@@ -15,30 +15,31 @@ export default function HomeView(){
      <div className="flex flex-col items-center h-fit mt-10 md:w-full">
  <button className="fixed pt-70 bottom-10 right-10 hover:cursor-pointer" onClick={() => scrollToSection('top')}>{<ArrowUp className="border-2 border-[#cad2c5] text-[#cad2c5] w-7 h-7 rounded-sm"/>}</button>
         <section id="introduction" className="min-h-screen w-[80%] max-w-250">
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col md:flex-row md:pt-50 md:gap-5 items-center">
             <div className="flex flex-col items-center">{/*Name and information container*/}
-          <h1>Gustav Hugo Tonning Bøe</h1>
+          <h1 className="text-3xl">Gustav Hugo Tonning Bøe</h1>
            <div className="md:hidden overflow-clip rounded-[40%] border-5 border-[#cad2c5]">{/*Image container mobile*/}
         <img src="https://i.imghippo.com/files/SM8096ys.jpeg" alt="IMG_9270" className="object-bottom h-70 w-70 "/>
         </div>
           <h2>Junior Frontend developer</h2>
           <div className="flex flex-col items-center">{/*About me container */}
-            <h3>About me</h3>
-          <article className="p-5 rounded-md shadow-lg shadow-[#2f3e46] bg-[#52796f]/20">
+            
+          <article className="p-5 gap-5 rounded-md shadow-lg shadow-[#2f3e46] bg-[#52796f]/20">
+           <h3>About me</h3>
            <p>I am a 26 year old, newly educated frontend developer, based in Bergen together with my two lovely kids and girlfriend</p>
-           <p>In my projects, I strive to create websites with clean designs and great UI/UX</p>
+           <p>For my projects, I strive to create websites with clean designs and great UI/UX</p>
           </article>
           </div>
         </div>
         <div className=" hidden md:block rounded-[40%] border-5 border-[#cad2c5] overflow-clip">{/*Image container desktop*/}
-        <img src="https://i.imghippo.com/files/SM8096ys.jpeg" alt="IMG_9270" className="object-center w-65  "/>
+        <img src="https://i.imghippo.com/files/SM8096ys.jpeg" alt="IMG_9270" className="object-center w-150  "/>
         </div>
         <button className="mt-25 hover:animate-pulse hover:delay-300 hover:cursor-pointer" onClick={() => scrollToSection('projects')}>{<ChevronDown/>}</button>
        
           </div>
          </section>
 
-        <section id="projects" className="flex flex-col items-center w-full max-w-250 ">{/*Projects container */}
+        <section id="projects" className="flex flex-col items-center w-full ">{/*Projects container */}
          
           <div className="flex flex-col items-center min-h-screen mt-15 w-full">
              <h1 className="text-3xl mb-5 text-[#e9efe5] hover:cursor-pointer"  onClick={() => scrollToSection('projects')}>Projects</h1>
@@ -52,13 +53,14 @@ export default function HomeView(){
               <button className={viewProject==="frameworks" ? "hidden": "hover:cursor-pointer"} onClick={()=>{setViewProject("frameworks")}}>{<ArrowDown size={24} className="hover:cursor-pointer"/>}</button>
              </div>
               <div className={viewProject !== "frameworks" ? "hidden": " flex flex-col items-center w-full gap-5"}>
-                <div className="h-50 overflow-clip border-y-2">{/*Image container*/}
-                  <img src="https://i.imghippo.com/files/ZD7102TJs.png" alt="Css Frameworks preview" />
+                <div className="h-50 md:w-200 overflow-clip md:overflow-auto border-y-2">{/*Image container*/}
+                  <img src="https://i.imghippo.com/files/ZD7102TJs.png" alt="Css Frameworks preview" className="md:hidden"/>
+                  <img src="https://i.imghippo.com/files/IRJC9593T.png" alt="compressed_0_compressed_1780388720612_hgrfw4hvxie" className="hidden md:block"/>
                 </div>
                 <div className="flex flex-col items-center w-full gap-5">{/*Text container*/}
                   <h2 className="text-xl border-[#52796f] w-full">CSS Frameworks</h2>
                  
-                  <article className="text-left text-sm w-[90%]">A course assignement where the main goal was to style a couple of pages using a css framework of our choosing. </article>
+                  <article className="text-left md:text-center text-sm w-[90%]">A course assignement where the main goal was to style a couple of pages using a css framework of our choosing. </article>
                 </div>
                 <Link to={"/projects/CssFrameworks"} className="px-5 py-2 border text-lg mt-5 mb-10 ">View project</Link>
                 </div>
@@ -75,8 +77,9 @@ export default function HomeView(){
               <button className={viewProject==="project" && "hidden"} onClick={()=>{setViewProject("project")}}>{<ArrowDown size={24} className="hover:cursor-pointer"/>}</button>
              </div>
               <div className={viewProject !== "project" ? "hidden": " flex flex-col items-center w-full gap-5"}>
-                <div className="h-50 overflow-clip border-y-2">{/*Image container*/}
-                   <img src="https://i.imghippo.com/files/TnbA2911foY.png" alt="Semester project preview" />
+                <div className="h-50 md:w-200 overflow-clip md:overflow-auto border-y-2">{/*Image container*/}
+                   <img src="https://i.imghippo.com/files/TnbA2911foY.png" alt="Semester project preview" className="md:hidden"/>
+                   <img src="https://i.imghippo.com/files/byzK2264kJE.png" alt="compressed_1_compressed_1780388720612_ty9dewpi3df" className="hidden md:block"/>
                 </div>
                 <div className="flex flex-col items-center w-full gap-5">{/*Text container*/}
                   <h2 className="text-xl border-[#52796f] w-full">Semester Project</h2>
@@ -96,8 +99,9 @@ export default function HomeView(){
               <button className={viewProject==="exam" && "hidden"} onClick={()=>{setViewProject("exam")}}>{<ArrowDown size={24} className="hover:cursor-pointer"/>}</button>
              </div>
               <div className={viewProject !== "exam" ? "hidden": " flex flex-col items-center w-full gap-5"}>
-                <div className="h-50 overflow-clip border-y-2">{/*Image container*/}
-                   <img src="https://i.imghippo.com/files/xZVy5304GS.png" alt="Project exam preview" />
+                <div className="h-50 md:w-200 overflow-clip md:overflow-auto border-y-2">{/*Image container*/}
+                   <img src="https://i.imghippo.com/files/xZVy5304GS.png" alt="Project exam preview" className="md:hidden"/>
+                   <img src="https://i.imghippo.com/files/TdIV7114kuw.png" alt="compressed_2_compressed_1780388720612_xmfpl2o4gct" className="md:block"/>
                 </div>
                 <div className="flex flex-col items-center w-full gap-5">{/*Text container*/}
                   <h2 className="text-xl border-[#52796f] w-full">Project exam</h2>
@@ -115,7 +119,7 @@ export default function HomeView(){
         </div>
          
         </section>
-        <section id="contact" className="min-h-screen">
+        <section id="contact" className="min-h-screen pt-15">
           <h1 className="text-3xl mb-5 text-[#e9efe5] hover:cursor-pointer"  onClick={() => scrollToSection('contact')}>Contact me</h1>
           
         </section>
